78c0b06983669ceefafc33d661f793f6b6cc246f
0E9B061F <0E9B061F@protonmail.com>
2023-10-12 04:47:07 -0400

    cleanup and fix size command

--- spec/cli-spec.mjs
+++ spec/cli-spec.mjs
@@ -1,20 +1,17 @@
 import { prep } from "./prep.mjs"
 import { mkconf } from "../lib/conf.mjs"
 import path from "node:path"
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { fileURLToPath } from 'url'
 
-const pwfile = path.join(__dirname, "password")
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const root = path.join(__dirname, "data")
-const repo = path.join(root, "repo")
-const target = path.join(root, "target")
 const restore = path.join(root, "restore")
 
 describe("cli", ()=> {
   it("backup and restore with exclusions", async ()=> {
     const conf = mkconf("rcfile:./spec/scroll.yaml", "keyfile:./spec/password", "logfile:./spec/log")
     const t = await prep()
-    
+
     await conf.cmds.run("backup")
     await conf.cmds.run("restore", "test1", "local", restore)
 
