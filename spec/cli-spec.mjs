b10f5541979b8f46cc909cd2a405ee6937748aec
0E9B061F <0E9B061F@protonmail.com>
2024-05-22 04:26:27 -0400

    code cleanup

--- spec/cli-spec.mjs
+++ spec/cli-spec.mjs
@@ -1,8 +1,9 @@
-/* scroll backup system
-   by 0E9B061F <0E9B061F@protonmail.com> 2020-2024
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at https://mozilla.org/MPL/2.0/.
+/* scroll backup system                                     ____  __ ____  __  
+   by 0E9B061F <0E9B061F@protonmail.com>                   |    \|  |    \|  | 
+This Source Code Form  is subject to  the terms of        ====\  \=====\  \====
+the  Mozilla Public License,  v. 2.0. If a copy of         |__|\____|__|\____| 
+the MPL  was not distributed  with this  file, You             2020 - 2024
+can obtain one at https://mozilla.org/MPL/2.0/.                
 */
 
 import { prep } from "./prep.mjs"
