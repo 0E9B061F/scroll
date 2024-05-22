15323831b3970c19f8975ab59b469d4559a5d129
0E9B061F <0E9B061F@protonmail.com>
2024-05-22 00:34:56 -0400

    added  command

--- spec/cli-spec.mjs
+++ spec/cli-spec.mjs
@@ -1,16 +1,21 @@
 import { prep } from "./prep.mjs"
 import { mkconf } from "../lib/conf.mjs"
 import { dirname, join } from "node:path"
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'node:url'
+import { hostname } from "node:os"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const root = join(__dirname, "data")
 const restorea = join(root, "restore-a")
 const restoreb = join(root, "restore-b")
+const synca = join(root, "sync-a")
+const syncaRoot = join(synca, hostname(), "test2")
+const syncb = join(root, "sync-b")
+const syncbRoot = join(syncb, hostname(), "test3")
 
 describe("cli", ()=> {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
-  it("backup and restore with exclusions", async ()=> {
+  it("backup and restore from restic repos with exclusions", async ()=> {
     const conf = mkconf("rcfile:./spec/scroll.yaml", "keyfile:./spec/password", "logdir:./spec")
     const t = await prep()
 
@@ -33,6 +38,20 @@ describe("cli", ()=> {
 
     expect(m1).toEqual([])
     expect(m2).toEqual([])
+  })
+  it("can run backups to rsync backends", async()=> {
+    const conf = mkconf("rcfile:./spec/scroll.yaml", "keyfile:./spec/password", "logdir:./spec")
+    const t = await prep()
+
+    await conf.cmds.run("backup", "test2", "sync-a")
+
+    const m1 = await t.match(syncaRoot, {
+      dir1: false,
+      dir2: false,
+      dir8: false,
+    })
+
+    expect(m1).toEqual([])
   })
 })
 
